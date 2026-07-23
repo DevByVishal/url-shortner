3fa415a52303a735429ec05fcb71ba1b1c58388b
DevByVishal <vm9154308@gmail.com>
2026-07-23 07:34:30 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,14 +8,22 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index()
-    {
-        $users = User::where(
-            'company_id',
-            auth()->user()->company_id
-        )
-        ->latest()
-        ->paginate(10);
+{
+    $user = auth()->user();
 
-        return view('users.index', compact('users'));
+    if ($user->hasRole('SuperAdmin')) {
+        // SuperAdmin can see all users
+        $users = User::with('company')->latest()->get();
+    } elseif ($user->hasRole('Admin')) {
+        // Admin can see users from their own company
+        $users = User::with('company')
+            ->where('company_id', $user->company_id)
+            ->latest()
+            ->get();
+    } else {
+        abort(403);
     }
+
+    return view('users.index', compact('users'));
+}
 }
